14e638d05b50884f31bcdf475128303a82b0d5a3
keepcalmandcodecodecode <easy.dev@yandex.ru>
2016-08-06 20:13:38 +0300

    class methods

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -21,10 +21,12 @@ describe(@"these will pass", ^{
     it(@"classname from NSObject", ^{
         NSObject *someObject = [NSObject new];
         expect(NSStringFromClass([someObject class])).to.equal([someObject rmx_classname]);
+        expect(NSStringFromClass([someObject class])).to.equal([someObject.class rmx_classname]);
     });
     it(@"classname from NSProxy", ^{
         SomeNSProxyClass *someNSProxyObject = [[SomeNSProxyClass alloc] init];
         expect(NSStringFromClass([someNSProxyObject class])).to.equal([someNSProxyObject rmx_classname]);
+        expect(NSStringFromClass([someNSProxyObject class])).to.equal([someNSProxyObject.class rmx_classname]);
     });
 });
 
